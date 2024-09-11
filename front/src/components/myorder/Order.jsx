c1d7a96379a0c51f681fd52d11c7c059845ec727
junho <>
2024-09-11 16:25:36 +0900

    feat: 주문 상태를 원활히 보여주는 코드 추가

--- front/src/components/myorder/Order.jsx
+++ front/src/components/myorder/Order.jsx
@@ -2,12 +2,13 @@ import React from 'react'
 import Item from './Item'
 
 const Order = ({ data }) => {
+    const status = data.orderStatus === 'ORDERED' ? '주문 완료' : '배송 진행 중';
   return (
     <div>
         <h6>{data?.email}</h6>
         <h6>{data?.address}</h6>
         <h6>{data?.postcode}</h6>
-        <h6>{data?.orderStatus}</h6>
+        <h6>{status}</h6>
         {
             data?.orderDetails.map((per,index) => 
                 <Item key={index} item={per}/>
