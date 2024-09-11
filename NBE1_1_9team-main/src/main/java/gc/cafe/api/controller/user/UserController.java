248b864e1ae7374fb6a8730a2668722d63339511
Taaate <an981022@naver.com>
2024-09-11 16:16:04 +0900

    fix: #3 요청 url 변경

--- NBE1_1_9team-main/src/main/java/gc/cafe/api/controller/user/UserController.java
+++ NBE1_1_9team-main/src/main/java/gc/cafe/api/controller/user/UserController.java
@@ -18,7 +18,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/users")
+@RequestMapping("/api/v1/users")
 @RequiredArgsConstructor
 public class UserController {
 
