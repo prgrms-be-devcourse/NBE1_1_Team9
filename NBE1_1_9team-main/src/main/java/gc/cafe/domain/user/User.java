951df92cc6121ff9937b19a7366bd46818d491a2
Taaate <an981022@naver.com>
2024-09-11 10:32:19 +0900

    feat: #3 유저 관련 엔티티 설계

---
+++ NBE1_1_9team-main/src/main/java/gc/cafe/domain/user/User.java
@@ -0,0 +1,31 @@
+package gc.cafe.domain.user;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class User {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
+    private Long id;
+
+    private String email;
+    private String password;
+
+    private String name;
+
+    public User(String email, String password, String name) {
+        this.email = email;
+        this.password = password;
+        this.name = name;
+    }
+}
